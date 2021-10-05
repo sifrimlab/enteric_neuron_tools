81f7abe8db365061803ab5a365dad8e9194abfb1
WoutDavid <wouters.david@hotmail.com>
2021-10-05 11:02:56 +0200

    started adapting the code to not need java vm anymore

--- COUNTEN/count_ent_neurons.py
+++ COUNTEN/count_ent_neurons.py
@@ -3,12 +3,13 @@
 import os
 import sys
 import warnings
+from skimage import io
 import importlib.util # This is to manually import the scripts to make sure it's all correct
 warnings.filterwarnings('ignore')
 import javabridge
-from icecream import ic
 import bioformats
 import matplotlib.pyplot as plt
+import time
 
 # IO
 image = sys.argv[1]
@@ -23,15 +24,19 @@ pixel_density = 3.2
 sigma = 7
 min_samples = 2
 
-spec = importlib.util.spec_from_file_location("io",os.path.join(script_path, "io.py"))
-io = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(io)
+# spec = importlib.util.spec_from_file_location("io",os.path.join(script_path, "custom_io.py"))
+# custom_io = importlib.util.module_from_spec(spec)
+# spec.loader.exec_module(custom_io)
 
-javabridge.start_vm(class_path=bioformats.JARS)
+# javabridge.start_vm(class_path=bioformats.JARS)
 
-neurons, directory, meta = io.load_TIFF(image, output_dir)
+# neurons, directory, meta = custom_io.load_TIFF(image, output_dir)
+# # print(neurons.type)
 
-javabridge.kill_vm()
+# javabridge.kill_vm()
+neurons = io.imread(image)
+meta = {"Name": os.path.splitext(image)[0]}
+directory = os.path.join(str(output_dir), f"result_{os.path.splitext(meta['Name'])[0]}_{time.strftime('%m'+'_'+'%d'+'_'+'%Y')}")
 
 # import another module
 spec = importlib.util.spec_from_file_location("processing",os.path.join(script_path,"processing.py"))
