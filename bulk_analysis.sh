aa43cfe00b4a5b493daa962eebfb59668f8d07f7
WoutDavid <wouters.david@hotmail.com>
2022-06-14 16:37:28 +0200

    updated bulk

--- bulk_analysis.sh
+++ bulk_analysis.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 
-FILES="./data/confocal_mouse_stomachs/*.czi"
+FILES="./data/confocal_mouse_stomachs/redo/*.czi"
 for f in $FILES
 do
   echo "Processing $f file..."
-  python extract_and_count.py -o ./mouse_stomach_output/ -i -c 0 -p 5 -s 7 -m 2 $f
+  python extract_and_count.py -o ./mouse_stomach_output/redo/ -i -c 0 -p 5 -s 17 -m 2 $f
 done
